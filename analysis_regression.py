cacca25a8454a74db0e59e017558a73751b80c1d
Andrew Stirn <andrew.stirn@cs.columbia.edu>
2022-05-16 21:52:15 -0700

    tables folder

--- analysis_regression.py
+++ analysis_regression.py
@@ -5,8 +5,8 @@ import pandas as pd
 from data_regression import REGRESSION_DATA
 from utils import build_table, champions_club_table
 
-if not os.path.exists('assets'):
-    os.mkdir('assets')
+if not os.path.exists('tables'):
+    os.mkdir('tables')
 
 results = dict()
 for dataset in REGRESSION_DATA.keys():
@@ -21,12 +21,12 @@ max_cols = 5
 champions_club = []
 for metric in ['LL', 'RMSE', 'ECE']:
     order = 'max' if metric == 'LL' else 'min'
-    with open(os.path.join('assets', 'regression_uci_' + metric.lower().replace(' ', '_') + '.tex'), 'w') as f:
-        table, cc = build_table(results, metric, order, max_cols, bold_statistical_ties=False)
+    with open(os.path.join('tables', 'regression_uci_' + metric.lower().replace(' ', '_') + '.tex'), 'w') as f:
+        table, cc = build_table(results, metric, order, max_cols, bold_statistical_ties=True)
         print(table.replace('NaN', '--'), file=f)
     champions_club.append(cc)
 
 
 # print champions club
-with open(os.path.join('assets', 'regression_uci_champions_club.tex'), 'w') as f:
+with open(os.path.join('tables', 'regression_uci_champions_club.tex'), 'w') as f:
     print(champions_club_table(champions_club), file=f)
