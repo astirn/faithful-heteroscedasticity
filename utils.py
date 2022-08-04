b21857aafa2ac87e920b24c83dcca2ed949ecd64
Andrew Stirn <andrew.stirn@cs.columbia.edu>
2022-08-03 20:40:25 -0700

    ZScoreNormalization class

--- utils.py
+++ utils.py
@@ -1,7 +1,36 @@
 import os
 import pickle
 import pandas as pd
+import tensorflow as tf
 from scipy.stats import ks_2samp
+
+
+class ZScoreNormalization(object):
+    def __init__(self, y_mean, y_var):
+        self.y_mean = tf.constant(y_mean, dtype=tf.float32)
+        self.y_var = tf.constant(y_var, dtype=tf.float32)
+        self.y_std = tf.sqrt(self.y_var)
+
+    def normalize_targets(self, y):
+        return (y - self.y_mean) / self.y_std
+
+    def scale_parameters(self, name, value):
+        if name == 'mean':
+            return value * self.y_std + self.y_mean
+        elif name == 'std':
+            return value * self.y_std
+        elif name == 'var':
+            return value * self.y_var
+        elif name == 'y':
+            return value
+        else:
+            raise NotImplementedError
+
+    # def de_whiten_precision(self, precision):
+    #     return precision / self.y_var
+    #
+    # def de_whiten_log_precision(self, log_precision):
+    #     return log_precision - tf.math.log(self.y_var)
 
 
 def string_table(df):
